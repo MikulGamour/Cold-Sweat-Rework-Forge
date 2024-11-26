f13e4c39b4ad5c2bff7def16add9788bceb4f76b
MikulDev <gamer3mc@gmail.com>
2024-11-25 20:52:25 -0500

    Use goat age instead of ticksExisted for fur growth
    Took 10 seconds
    
    Took 17 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ShearableFurManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ShearableFurManager.java
@@ -107,7 +107,7 @@ public class ShearableFurManager
             {   entity.getPersistentData().putInt("FurGrowthCooldown", entity.getPersistentData().getInt("FurGrowthCooldown") - 1);
             }
             // Entity is shearable, current tick is a multiple of the regrow time, and random chance succeeds
-            if (!entity.level.isClientSide && entity.tickCount % furConfig.getA() == 0 && Math.random() < furConfig.getC())
+            if (!entity.level.isClientSide && entity.getAge() % Math.max(1, furConfig.getA()) == 0 && Math.random() < furConfig.getC())
             {
                 getFurCap(entity).ifPresent(cap ->
                 {
