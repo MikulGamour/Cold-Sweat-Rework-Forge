7b15502b6ba2b204fef4f9756b9ce332720fc196
MikulDev <gamer3mc@gmail.com>
2024-12-03 19:21:51 -0500

    Fix incorrect null check when checking for insulation item in SewingContainer
    Took 1 second

--- src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
+++ src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
@@ -68,7 +68,7 @@ public class SewingContainer extends AbstractContainerMenu
 
         @Override
         public boolean isEmpty()
-        {   return !stackList.stream().anyMatch(stack -> !stack.isEmpty());
+        {   return stackList.stream().allMatch(ItemStack::isEmpty);
         }
 
         @Nonnull
@@ -303,7 +303,7 @@ public class SewingContainer extends AbstractContainerMenu
                 });
             }
             // Item is for insulation
-            else if (ConfigSettings.INSULATION_ITEMS.get().get(insulatorItem.getItem()) != null
+            else if (!ConfigSettings.INSULATION_ITEMS.get().get(insulatorItem.getItem()).isEmpty()
             && (!(insulatorItem.getItem() instanceof Wearable)
             || LivingEntity.getEquipmentSlotForItem(wearableItem) == LivingEntity.getEquipmentSlotForItem(insulatorItem)))
             {
