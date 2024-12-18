5050654e2752e0a5f210b46eda1e53d634c5902d
MikulDev <gamer3mc@gmail.com>
2024-12-18 16:00:31 -0500

    Remove unnecessary fully-qualified class references

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/SpawnBiomeData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/SpawnBiomeData.java
@@ -44,7 +44,7 @@ public class SpawnBiomeData extends ConfigData
 
     public static final Codec<SpawnBiomeData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ConfigHelper.tagOrHolderCodec(Registry.BIOME_REGISTRY, Biome.CODEC).listOf().fieldOf("biomes").forGetter(SpawnBiomeData::biomes),
-            net.minecraft.world.entity.MobCategory.CODEC.fieldOf("category").forGetter(SpawnBiomeData::category),
+            MobCategory.CODEC.fieldOf("category").forGetter(SpawnBiomeData::category),
             Codec.INT.fieldOf("weight").forGetter(SpawnBiomeData::weight),
             ConfigHelper.tagOrBuiltinCodec(Registry.ENTITY_TYPE_REGISTRY, ForgeRegistries.ENTITY_TYPES).listOf().fieldOf("entities").forGetter(SpawnBiomeData::entities),
             Codec.STRING.listOf().optionalFieldOf("required_mods", List.of()).forGetter(SpawnBiomeData::requiredMods)
