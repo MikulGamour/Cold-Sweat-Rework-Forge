7f49fb93a2639057b83f2499af62571479fddfb3
MikulDev <gamer3mc@gmail.com>
2024-12-18 22:15:58 -0500

    Insulator tooltip fixes/tweaks
    Took 37 seconds

--- src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
@@ -69,7 +69,7 @@ public class TooltipHandler
 
     private static int HOVERED_ITEM_UPDATE_COOLDOWN = 0;
     private static ItemStack HOVERED_STACK = ItemStack.EMPTY;
-    public static FastMap<UUID, Boolean> HOVERED_STACK_PREDICATES = new FastMap<>();
+    public static HashMap<UUID, Boolean> HOVERED_STACK_PREDICATES = new HashMap<>();
 
     public static <T extends ConfigData> boolean passesRequirement(T element)
     {   return HOVERED_STACK_PREDICATES.getOrDefault(element.getId(), true);
@@ -113,7 +113,7 @@ public class TooltipHandler
         return tooltipEndIndex;
     }
 
-    public static void addModifierTooltipLines(List<ITextComponent> tooltip, AttributeModifierMap map, boolean strikethrough)
+    public static void addModifierTooltipLines(List<ITextComponent> tooltip, AttributeModifierMap map, boolean showIcon, boolean strikethrough)
     {
         map.getMap().asMap().forEach((attribute, modifiers) ->
         {
@@ -124,7 +124,7 @@ public class TooltipHandler
                 {   value += modifier.getAmount();
                 }
                 if (value != 0)
-                {   tooltip.add(getFormattedAttributeModifier(attribute, value, operation, false, strikethrough));
+                {   tooltip.add(getFormattedAttributeModifier(attribute, value, operation, showIcon, strikethrough));
                 }
             }
         });
--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinItemTooltip.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinItemTooltip.java
@@ -52,8 +52,8 @@ public class MixinItemTooltip
         {
             tooltip.add(new StringTextComponent(""));
             tooltip.add(new TranslationTextComponent("item.modifiers.insulation").withStyle(TextFormatting.GRAY));
-            TooltipHandler.addModifierTooltipLines(tooltip, insulatorAttributes, false);
-            TooltipHandler.addModifierTooltipLines(tooltip, unmetInsulatorAttributes, true);
+            TooltipHandler.addModifierTooltipLines(tooltip, insulatorAttributes, true, false);
+            TooltipHandler.addModifierTooltipLines(tooltip, unmetInsulatorAttributes, true, true);
         }
 
         // Add curio attributes to tooltip
@@ -70,8 +70,8 @@ public class MixinItemTooltip
         {
             tooltip.add(new StringTextComponent(""));
             tooltip.add(new TranslationTextComponent("item.modifiers.curio").withStyle(TextFormatting.GRAY));
-            TooltipHandler.addModifierTooltipLines(tooltip, curioAttributes, false);
-            TooltipHandler.addModifierTooltipLines(tooltip, unmetCurioAttributes, true);
+            TooltipHandler.addModifierTooltipLines(tooltip, curioAttributes, true, false);
+            TooltipHandler.addModifierTooltipLines(tooltip, unmetCurioAttributes, true, true);
         }
     }
 
