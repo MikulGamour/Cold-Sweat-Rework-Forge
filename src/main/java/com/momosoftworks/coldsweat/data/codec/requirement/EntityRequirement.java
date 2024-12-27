909befe4d386963da57dd7cdfe58fdfd0fa448df
MikulDev <gamer3mc@gmail.com>
2024-12-27 00:29:17 -0500

    Use null entity predicate by default for KubeJS builders

--- src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/EntityRequirement.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/EntityRequirement.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,12 +41,12 @@ public record EntityRequirement(Optional<List<Either<TagKey<EntityType<?>>, Enti
              Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
     }
 
-    public EntityRequirement(Predicate<Entity> predicate)
+    public EntityRequirement(@Nullable Predicate<Entity> predicate)
     {
         this(Optional.empty(), Optional.empty(), Optional.empty(),
              Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
              Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
-             Optional.of(predicate));
+             Optional.ofNullable(predicate));
     }
 
     public static final EntityRequirement NONE = new EntityRequirement(Optional.empty(), Optional.empty(), Optional.empty(),
