206c2150429491da6e913c63b40d154f3a547f17
MikulDev <gamer3mc@gmail.com>
2024-12-30 08:51:16 -0500

    Add VS support to ElevationTempModifier skylight check
    Took 2 seconds

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
@@ -2,6 +2,7 @@ package com.momosoftworks.coldsweat.api.temperature.modifier;
 
 import com.mojang.datafixers.util.Pair;
 import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.compat.CompatManager;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.data.codec.configuration.BiomeTempData;
 import com.momosoftworks.coldsweat.data.codec.configuration.DepthTempData;
@@ -43,7 +44,8 @@ public class ElevationTempModifier extends TempModifier
             depthTable.add(Pair.of(pos, CSMath.getDistance(entity.blockPosition(), pos)));
         }
 
-        int skylight = entity.level.getBrightness(LightLayer.SKY, entity.blockPosition());
+        BlockPos translatedPos = CompatManager.Valkyrien.transformIfShipPos(level, entity.blockPosition());
+        int skylight = entity.level.getBrightness(LightLayer.SKY, translatedPos);
 
         Map<BlockPos, Pair<DepthTempData.TempRegion, Double>> depthRegions = new FastMap<>();
 
