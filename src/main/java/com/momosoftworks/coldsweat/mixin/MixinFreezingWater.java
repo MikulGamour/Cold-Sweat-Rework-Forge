41cd097acdcce4e8429255751b7c5cef5283b274
MikulDev <gamer3mc@gmail.com>
2024-12-31 10:54:17 -0500

    Fix div/0 at tickspeed less than 3

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinFreezingWater.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinFreezingWater.java
@@ -70,7 +70,7 @@ public class MixinFreezingWater
 
             int tickSpeed = self.getGameRules().getInt(GameRules.RULE_RANDOMTICKING);
             if (tickSpeed == 0) return 999999;
-            return Math.max(1, bound / (tickSpeed / 3));
+            return Math.max(1, bound / Math.max(1, tickSpeed / 3));
         }
     }
 
