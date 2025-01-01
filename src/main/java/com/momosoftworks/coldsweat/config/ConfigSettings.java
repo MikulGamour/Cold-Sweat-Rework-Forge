Yorunina/Cold-Sweat

Add drying items config setting & generify registry creation

src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -36,7 +36,6 @@ import net.minecraftforge.fml.util.thread.EffectiveSide;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.util.TriConsumer;
 import org.joml.Vector2i;
-import oshi.util.tuples.Triplet;
 
 import java.util.*;
 import java.util.function.*;
@@ -127,6 +126,7 @@ public class ConfigSettings
     public static final DynamicHolder<Multimap<Item, InsulatorData>> INSULATING_CURIOS;
     public static final DynamicHolder<ScalingFormula> INSULATION_SLOTS;
     public static final DynamicHolder<List<Item>> INSULATION_BLACKLIST;
+    public static final DynamicHolder<Map<Item, DryingItemData>> DRYING_ITEMS;
 
     public static final DynamicHolder<Multimap<Item, FoodData>> FOOD_TEMPERATURES;
 
@@ -490,6 +490,30 @@ public class ConfigSettings
                                                     .stream()
                                                     .map(entry -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(entry)))
                                                     .collect(ArrayList::new, List::add, List::addAll)));
+
+        DRYING_ITEMS = addSyncedSetting("drying_items", FastMap::new, holder ->
+        {
+            Map<Item, DryingItemData> dataMap = new FastMap<>();
+            for (List<?> entry : ItemSettingsConfig.DRYING_ITEMS.get())
+            {
+                DryingItemData data = DryingItemData.fromToml(entry);
+                if (data == null) continue;
+
+                data.setType(ConfigData.Type.TOML);
+
+                for (Item item : RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, CSMath.listOrEmpty(data.data().items())))
+                {   dataMap.put(item, data);
+                }
+            }
+            // Handle registry removals
+            ConfigLoadingHandler.removeEntries(dataMap.values(), ModRegistries.DRYING_ITEM_DATA);
+            // Add entries
+            holder.get().putAll(dataMap);
+        },
+        (encoder) -> ConfigHelper.serializeRegistry(encoder, "DryingItems", Registries.ITEM, ModRegistries.DRYING_ITEM_DATA, ForgeRegistries.ITEMS::getKey),
+        (decoder) -> ConfigHelper.deserializeRegistry(decoder, "DryingItems", ModRegistries.DRYING_ITEM_DATA, rl -> ForgeRegistries.ITEMS.getValue(rl)),
+        (saver) -> {},
+        SyncType.ONE_WAY);
 
         CHECK_SLEEP_CONDITIONS = addSetting("check_sleep_conditions", () -> true, holder -> holder.set(WorldSettingsConfig.SHOULD_CHECK_SLEEP.get()));
 

src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/DryingItemData.java
@@ -1,0 +1,104 @@
+package com.momosoftworks.coldsweat.data.codec.configuration;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.impl.RequirementHolder;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
+import com.momosoftworks.coldsweat.data.codec.requirement.NbtRequirement;
+import com.momosoftworks.coldsweat.util.math.CSMath;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.ListBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DryingItemData extends ConfigData implements RequirementHolder
+{
+    public static final Codec<DryingItemData> CODEC = RecordCodecBuilder.create(builder -> builder.group(
+            ItemRequirement.CODEC.fieldOf("item").forGetter(data -> data.data),
+            ItemStack.CODEC.optionalFieldOf("result", ItemStack.EMPTY).forGetter(data -> data.result),
+            EntityRequirement.getCodec().optionalFieldOf("entity", EntityRequirement.NONE).forGetter(data -> data.entity),
+            SoundEvent.DIRECT_CODEC.optionalFieldOf("sound", SoundEvents.WET_GRASS_STEP).forGetter(data -> data.sound)
+    ).apply(builder, DryingItemData::new));
+
+    private final ItemRequirement data;
+    private final ItemStack result;
+    private final EntityRequirement entity;
+    private final SoundEvent sound;
+
+    public DryingItemData(ItemRequirement data, ItemStack result, EntityRequirement entity, SoundEvent sound,
+                          List<String> requiredMods)
+    {
+        super(requiredMods);
+        this.data = data;
+        this.result = result;
+        this.entity = entity;
+        this.sound = sound;
+    }
+
+    public DryingItemData(ItemRequirement data, ItemStack result, EntityRequirement entity, SoundEvent sound)
+    {
+        this(data, result, entity, sound,
+             // Required mods
+             ListBuilder.begin(ConfigHelper.getModIDs(CSMath.listOrEmpty(data.items()), ForgeRegistries.ITEMS))
+             .add(ForgeRegistries.ITEMS.getKey(result.getItem()).getNamespace())
+             .add(sound.getLocation().getNamespace())
+             .build());
+    }
+
+    public ItemRequirement data()
+    {   return data;
+    }
+    public ItemStack result()
+    {   return result;
+    }
+    public EntityRequirement entity()
+    {   return entity;
+    }
+    public SoundEvent sound()
+    {   return sound;
+    }
+
+    @Nullable
+    public static DryingItemData fromToml(List<?> entry)
+    {
+        if (entry.size() < 2) return null;
+
+        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation((String) entry.get(0)));
+        Item result = ForgeRegistries.ITEMS.getValue(new ResourceLocation((String) entry.get(1)));
+        ResourceLocation sound = entry.size() > 2
+                                 ? new ResourceLocation((String) entry.get(2))
+                                 : new ResourceLocation("minecraft:block.wet_grass.step");
+
+        if (item != null && result != null)
+        {   ItemRequirement input = new ItemRequirement(List.of(Either.right(item)), new NbtRequirement());
+            return new DryingItemData(input, new ItemStack(result), EntityRequirement.NONE, ForgeRegistries.SOUND_EVENTS.getValue(sound));
+        }
+        else return null;
+    }
+
+    @Override
+    public boolean test(Entity entity)
+    {   return this.entity.test(entity);
+    }
+
+    @Override
+    public boolean test(ItemStack stack)
+    {   return this.data.test(stack, true);
+    }
+
+    @Override
+    public Codec<? extends ConfigData> getCodec()
+    {   return CODEC;
+    }
+}

MikulGamour/Cold-Sweat-Rework-Forge

src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -36,7 +36,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.util.thread.EffectiveSide;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.util.TriConsumer;
-import oshi.util.tuples.Triplet;
 
 import java.util.*;
 import java.util.function.*;
@@ -127,6 +126,7 @@ public class ConfigSettings
     public static final DynamicHolder<Multimap<Item, InsulatorData>> INSULATING_CURIOS;
     public static final DynamicHolder<ScalingFormula> INSULATION_SLOTS;
     public static final DynamicHolder<List<Item>> INSULATION_BLACKLIST;
+    public static final DynamicHolder<Map<Item, DryingItemData>> DRYING_ITEMS;
 
     public static final DynamicHolder<Multimap<Item, FoodData>> FOOD_TEMPERATURES;
 
@@ -490,6 +490,30 @@ public class ConfigSettings
                                                     .stream()
                                                     .map(entry -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(entry)))
                                                     .collect(ArrayList::new, List::add, List::addAll)));
+
+        DRYING_ITEMS = addSyncedSetting("drying_items", FastMap::new, holder ->
+        {
+            Map<Item, DryingItemData> dataMap = new FastMap<>();
+            for (List<?> entry : ItemSettingsConfig.DRYING_ITEMS.get())
+            {
+                DryingItemData data = DryingItemData.fromToml(entry);
+                if (data == null) continue;
+
+                data.setType(ConfigData.Type.TOML);
+
+                for (Item item : RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, CSMath.listOrEmpty(data.data().items())))
+                {   dataMap.put(item, data);
+                }
+            }
+            // Handle registry removals
+            ConfigLoadingHandler.removeEntries(dataMap.values(), ModRegistries.DRYING_ITEM_DATA);
+            // Add entries
+            holder.get().putAll(dataMap);
+        },
+        (encoder) -> ConfigHelper.serializeRegistry(encoder, "DryingItems", Registry.ITEM_REGISTRY, ModRegistries.DRYING_ITEM_DATA, ForgeRegistries.ITEMS::getKey),
+        (decoder) -> ConfigHelper.deserializeRegistry(decoder, "DryingItems", ModRegistries.DRYING_ITEM_DATA, rl -> ForgeRegistries.ITEMS.getValue(rl)),
+        (saver) -> {},
+        SyncType.ONE_WAY);
 
         CHECK_SLEEP_CONDITIONS = addSetting("check_sleep_conditions", () -> true, holder -> holder.set(WorldSettingsConfig.SHOULD_CHECK_SLEEP.get()));
 

src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/DryingItemData.java
@@ -1,0 +1,104 @@
+package com.momosoftworks.coldsweat.data.codec.configuration;
+
+import com.mojang.datafixers.util.Either;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.impl.RequirementHolder;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
+import com.momosoftworks.coldsweat.data.codec.requirement.NbtRequirement;
+import com.momosoftworks.coldsweat.util.math.CSMath;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.ListBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DryingItemData extends ConfigData implements RequirementHolder
+{
+    public static final Codec<DryingItemData> CODEC = RecordCodecBuilder.create(builder -> builder.group(
+            ItemRequirement.CODEC.fieldOf("item").forGetter(data -> data.data),
+            ItemStack.CODEC.optionalFieldOf("result", ItemStack.EMPTY).forGetter(data -> data.result),
+            EntityRequirement.getCodec().optionalFieldOf("entity", EntityRequirement.NONE).forGetter(data -> data.entity),
+            SoundEvent.CODEC.optionalFieldOf("sound", SoundEvents.WET_GRASS_STEP).forGetter(data -> data.sound)
+    ).apply(builder, DryingItemData::new));
+
+    private final ItemRequirement data;
+    private final ItemStack result;
+    private final EntityRequirement entity;
+    private final SoundEvent sound;
+
+    public DryingItemData(ItemRequirement data, ItemStack result, EntityRequirement entity, SoundEvent sound,
+                          List<String> requiredMods)
+    {
+        super(requiredMods);
+        this.data = data;
+        this.result = result;
+        this.entity = entity;
+        this.sound = sound;
+    }
+
+    public DryingItemData(ItemRequirement data, ItemStack result, EntityRequirement entity, SoundEvent sound)
+    {
+        this(data, result, entity, sound,
+             // Required mods
+             ListBuilder.begin(ConfigHelper.getModIDs(CSMath.listOrEmpty(data.items()), ForgeRegistries.ITEMS))
+             .add(ForgeRegistries.ITEMS.getKey(result.getItem()).getNamespace())
+             .add(sound.getLocation().getNamespace())
+             .build());
+    }
+
+    public ItemRequirement data()
+    {   return data;
+    }
+    public ItemStack result()
+    {   return result;
+    }
+    public EntityRequirement entity()
+    {   return entity;
+    }
+    public SoundEvent sound()
+    {   return sound;
+    }
+
+    @Nullable
+    public static DryingItemData fromToml(List<?> entry)
+    {
+        if (entry.size() < 2) return null;
+
+        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation((String) entry.get(0)));
+        Item result = ForgeRegistries.ITEMS.getValue(new ResourceLocation((String) entry.get(1)));
+        ResourceLocation sound = entry.size() > 2
+                                 ? new ResourceLocation((String) entry.get(2))
+                                 : new ResourceLocation("minecraft:block.wet_grass.step");
+
+        if (item != null && result != null)
+        {   ItemRequirement input = new ItemRequirement(List.of(Either.right(item)), new NbtRequirement());
+            return new DryingItemData(input, new ItemStack(result), EntityRequirement.NONE, ForgeRegistries.SOUND_EVENTS.getValue(sound));
+        }
+        else return null;
+    }
+
+    @Override
+    public boolean test(Entity entity)
+    {   return this.entity.test(entity);
+    }
+
+    @Override
+    public boolean test(ItemStack stack)
+    {   return this.data.test(stack, true);
+    }
+
+    @Override
+    public Codec<? extends ConfigData> getCodec()
+    {   return CODEC;
+    }
+}