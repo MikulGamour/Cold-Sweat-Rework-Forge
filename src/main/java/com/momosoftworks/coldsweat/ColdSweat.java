Yorunina/Cold-Sweat

Fix block temperature units implementation

src/main/java/com/momosoftworks/coldsweat/ColdSweat.java
@@ -25,6 +25,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.registries.DataPackRegistryEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -91,6 +92,10 @@ public class ColdSweat
             event.dataPackRegistry(ModRegistries.ENTITY_TEMP_DATA, EntityTempData.CODEC);
             event.dataPackRegistry(ModRegistries.REMOVE_REGISTRY_DATA, RemoveRegistryData.CODEC);
         });
+    }
+
+    public static String getVersion()
+    {   return FMLLoader.getLoadingModList().getModFileById(ColdSweat.MOD_ID).versionString();
     }
 
     public void commonSetup(final FMLCommonSetupEvent event)

MikulGamour/Cold-Sweat-Rework-Forge

src/main/java/com/momosoftworks/coldsweat/ColdSweat.java
@@ -22,6 +22,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
@@ -93,6 +94,10 @@ public class ColdSweat
             event.dataPackRegistry(ModRegistries.ENTITY_TEMP_DATA, EntityTempData.CODEC);
             event.dataPackRegistry(ModRegistries.REMOVE_REGISTRY_DATA, RemoveRegistryData.CODEC);
         });
+    }
+
+    public static String getVersion()
+    {   return FMLLoader.getLoadingModList().getModFileById(ColdSweat.MOD_ID).versionString();
     }
 
     public void commonSetup(final FMLCommonSetupEvent event)