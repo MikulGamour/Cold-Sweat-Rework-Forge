Yorunina/Cold-Sweat

Handle missing IDs in KubeJS scripts

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/BlockTempBuilderJS.java
@@ -1,22 +1,21 @@
 package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
 
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.temperature.block_temp.BlockTemp;
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import dev.latvian.mods.kubejs.level.BlockContainerJS;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class BlockTempBuilderJS
@@ -35,13 +34,7 @@ public class BlockTempBuilderJS
 
     public BlockTempBuilderJS blocks(String... blocks)
     {
-        this.blocks.addAll(Arrays.stream(blocks).map(key -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(key))).toList());
-        return this;
-    }
-
-    public BlockTempBuilderJS blockTag(String tag)
-    {
-        blocks.addAll(ForgeRegistries.BLOCKS.tags().getTag(TagKey.create(Registries.BLOCK, new ResourceLocation(tag))).stream().toList());
+        this.blocks.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.BLOCKS, ConfigHelper.getBlocks(blocks)));
         return this;
     }
 
@@ -78,6 +71,26 @@ public class BlockTempBuilderJS
     public BlockTempBuilderJS blockPredicate(Predicate<BlockContainerJS> predicate)
     {
         this.predicate = predicate;
+        return this;
+    }
+
+    public BlockTempBuilderJS state(String name, Object value)
+    {
+        Optional<Block> block = this.blocks.stream().findFirst();
+        if (block.isEmpty())
+        {   ColdSweat.LOGGER.error("No blocks have been added to this KubeJS block temp yet. Cannot add state check {{} = {}}", name, value);
+            return this;
+        }
+        // Get the property with the given name
+        Property<?> property = block.get().getStateDefinition().getProperty(name);
+        if (property != null)
+        {
+            // Parse the desired value for this property
+            property.getValue(value.toString()).ifPresent(propertyValue ->
+            {   // Append the new predicate to the existing one
+                predicate = predicate.and(blockJS -> blockJS.getBlockState().getValue(property) == propertyValue);
+            });
+        }
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FoodBuilderJS.java
@@ -4,9 +4,8 @@ import com.momosoftworks.coldsweat.data.codec.configuration.FoodData;
 import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
 import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -28,13 +27,7 @@ public class FoodBuilderJS
 
     public FoodBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(key))).toList());
-        return this;
-    }
-
-    public FoodBuilderJS itemTag(String tag)
-    {
-        items.addAll(ForgeRegistries.ITEMS.tags().getTag(TagKey.create(Registries.ITEM, new ResourceLocation(tag))).stream().toList());
+        this.items.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, ConfigHelper.getItems(items)));
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FuelBuilderJS.java
@@ -3,14 +3,12 @@ package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
 import com.momosoftworks.coldsweat.data.codec.configuration.FuelData;
 import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -26,13 +24,7 @@ public class FuelBuilderJS
 
     public FuelBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(key))).toList());
-        return this;
-    }
-
-    public FuelBuilderJS itemTag(String tag)
-    {
-        items.addAll(ForgeRegistries.ITEMS.tags().getTag(TagKey.create(Registries.ITEM, new ResourceLocation(tag))).stream().toList());
+        this.items.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, ConfigHelper.getItems(items)));
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/InsulatorBuilderJS.java
@@ -1,20 +1,25 @@
 package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
 
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.insulation.AdaptiveInsulation;
 import com.momosoftworks.coldsweat.api.insulation.Insulation;
 import com.momosoftworks.coldsweat.api.insulation.StaticInsulation;
+import com.momosoftworks.coldsweat.api.registry.TempModifierRegistry;
+import com.momosoftworks.coldsweat.compat.kubejs.util.KubeHelper;
 import com.momosoftworks.coldsweat.data.codec.configuration.InsulatorData;
 import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
 import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
 import com.momosoftworks.coldsweat.data.codec.util.AttributeModifierMap;
-import net.minecraft.core.registries.Registries;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.*;
@@ -35,13 +40,7 @@ public class InsulatorBuilderJS
 
     public InsulatorBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(key))).toList());
-        return this;
-    }
-
-    public InsulatorBuilderJS itemTag(String tag)
-    {
-        items.addAll(ForgeRegistries.ITEMS.tags().getTag(TagKey.create(Registries.ITEM, new ResourceLocation(tag))).stream().toList());
+        this.items.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, ConfigHelper.getItems(items)));
         return this;
     }
 
@@ -75,15 +74,24 @@ public class InsulatorBuilderJS
         return this;
     }
 
-    public InsulatorBuilderJS attribute(String attribute, double amount, String operation)
+    public InsulatorBuilderJS attribute(String attributeId, double amount, String operation)
     {
-        attributes.put(ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation(attribute)),
-                       new AttributeModifier("kubejs", amount, AttributeModifier.Operation.valueOf(operation.toUpperCase(Locale.ROOT))));
+        Attribute attribute = ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation(attributeId));
+        if (!KubeHelper.expect(attributeId, attribute, Attribute.class))
+        {   return this;
+        }
+        attributes.put(attribute, new AttributeModifier("kubejs", amount, AttributeModifier.Operation.valueOf(operation.toUpperCase(Locale.ROOT))));
         return this;
     }
 
     public InsulatorBuilderJS immuneToModifier(String modifierId, double immunity)
     {
+        ResourceLocation location = new ResourceLocation(modifierId);
+        if (!TempModifierRegistry.getEntries().containsKey(location))
+        {
+            ColdSweat.LOGGER.warn("Tried to add immunity to non-existent temperature modifier: {}", location);
+            return this;
+        }
         immuneTempModifiers.put(new ResourceLocation(modifierId), immunity);
         return this;
     }

MikulGamour/Cold-Sweat-Rework-Forge

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/BlockTempBuilderJS.java
@@ -1,8 +1,11 @@
 package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
 
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.temperature.block_temp.BlockTemp;
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import dev.latvian.mods.kubejs.level.BlockContainerJS;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -14,10 +17,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class BlockTempBuilderJS
@@ -36,13 +38,7 @@ public class BlockTempBuilderJS
 
     public BlockTempBuilderJS blocks(String... blocks)
     {
-        this.blocks.addAll(Arrays.stream(blocks).map(key -> BuiltInRegistries.BLOCK.get(ResourceLocation.parse(key))).toList());
-        return this;
-    }
-
-    public BlockTempBuilderJS blockTag(String tag)
-    {
-        blocks.addAll(BuiltInRegistries.BLOCK.getTag(TagKey.create(Registries.BLOCK, ResourceLocation.parse(tag))).orElseThrow().stream().map(Holder::value).toList());
+        this.blocks.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.BLOCK, ConfigHelper.getBlocks(blocks)));
         return this;
     }
 
@@ -79,6 +75,26 @@ public class BlockTempBuilderJS
     public BlockTempBuilderJS blockPredicate(Predicate<BlockContainerJS> predicate)
     {
         this.predicate = predicate;
+        return this;
+    }
+
+    public BlockTempBuilderJS state(String name, Object value)
+    {
+        Optional<Block> block = this.blocks.stream().findFirst();
+        if (block.isEmpty())
+        {   ColdSweat.LOGGER.error("No blocks have been added to this KubeJS block temp yet. Cannot add state check {{} = {}}", name, value);
+            return this;
+        }
+        // Get the property with the given name
+        Property<?> property = block.get().getStateDefinition().getProperty(name);
+        if (property != null)
+        {
+            // Parse the desired value for this property
+            property.getValue(value.toString()).ifPresent(propertyValue ->
+            {   // Append the new predicate to the existing one
+                predicate = predicate.and(blockJS -> blockJS.getBlockState().getValue(property) == propertyValue);
+            });
+        }
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FoodBuilderJS.java
@@ -6,9 +6,8 @@ import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
 import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -29,13 +28,7 @@ public class FoodBuilderJS
 
     public FoodBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(key))).toList());
-        return this;
-    }
-
-    public FoodBuilderJS itemTag(String tag)
-    {
-        items.addAll(BuiltInRegistries.ITEM.getTag(TagKey.create(Registries.ITEM, ResourceLocation.parse(tag))).orElseThrow().stream().map(Holder::value).toList());
+        this.items.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ITEM, ConfigHelper.getItems(items)));
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FuelBuilderJS.java
@@ -5,13 +5,11 @@ import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -27,13 +25,7 @@ public class FuelBuilderJS
 
     public FuelBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(key))).toList());
-        return this;
-    }
-
-    public FuelBuilderJS itemTag(String tag)
-    {
-        items.addAll(BuiltInRegistries.ITEM.getTag(TagKey.create(Registries.ITEM, ResourceLocation.parse(tag))).orElseThrow().stream().map(Holder::value).toList());
+        this.items.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ITEM, ConfigHelper.getItems(items)));
         return this;
     }
 

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/InsulatorBuilderJS.java
@@ -1,8 +1,11 @@
 package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
 
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.insulation.AdaptiveInsulation;
 import com.momosoftworks.coldsweat.api.insulation.Insulation;
 import com.momosoftworks.coldsweat.api.insulation.StaticInsulation;
+import com.momosoftworks.coldsweat.api.registry.TempModifierRegistry;
+import com.momosoftworks.coldsweat.compat.kubejs.util.KubeHelper;
 import com.momosoftworks.coldsweat.data.codec.configuration.InsulatorData;
 import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
@@ -10,13 +13,15 @@ import com.momosoftworks.coldsweat.data.codec.requirement.ItemRequirement;
 import com.momosoftworks.coldsweat.data.codec.util.AttributeModifierMap;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 import java.util.*;
 import java.util.function.Predicate;
@@ -36,13 +41,7 @@ public class InsulatorBuilderJS
 
     public InsulatorBuilderJS items(String... items)
     {
-        this.items.addAll(Arrays.stream(items).map(key -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(key))).toList());
-        return this;
-    }
-
-    public InsulatorBuilderJS itemTag(String tag)
-    {
-        items.addAll(BuiltInRegistries.ITEM.getTag(TagKey.create(Registries.ITEM, ResourceLocation.parse(tag))).orElseThrow().stream().map(Holder::value).toList());
+        this.items.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ITEM, ConfigHelper.getItems(items)));
         return this;
     }
 
@@ -76,15 +75,24 @@ public class InsulatorBuilderJS
         return this;
     }
 
-    public InsulatorBuilderJS attribute(String attribute, double amount, String operation)
+    public InsulatorBuilderJS attribute(String attributeId, double amount, String operation)
     {
-        attributes.put(BuiltInRegistries.ATTRIBUTE.get(ResourceLocation.parse(attribute)),
-                       new AttributeModifier(ResourceLocation.fromNamespaceAndPath("kubejs", "script"), amount, AttributeModifier.Operation.valueOf(operation.toUpperCase(Locale.ROOT))));
+        Holder<Attribute> attribute = BuiltInRegistries.ATTRIBUTE.getHolder(ResourceLocation.parse(attributeId)).orElse(null);
+        if (!KubeHelper.expect(attributeId, attribute, Holder.class))
+        {   return this;
+        }
+        attributes.put(attribute, new AttributeModifier(ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "kubejs"), amount, AttributeModifier.Operation.valueOf(operation.toUpperCase(Locale.ROOT))));
         return this;
     }
 
     public InsulatorBuilderJS immuneToModifier(String modifierId, double immunity)
     {
+        ResourceLocation location = ResourceLocation.parse(modifierId);
+        if (!TempModifierRegistry.getEntries().containsKey(location))
+        {
+            ColdSweat.LOGGER.warn("Tried to add immunity to non-existent temperature modifier: {}", location);
+            return this;
+        }
         immuneTempModifiers.put(ResourceLocation.parse(modifierId), immunity);
         return this;
     }