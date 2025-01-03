Yorunina/Cold-Sweat

Add drying items config setting & generify registry creation

src/main/java/com/momosoftworks/coldsweat/config/ConfigLoadingHandler.java
@@ -222,43 +222,62 @@ public class ConfigLoadingHandler
          Add JSON data to the config settings
          */
         // insulators
-        addInsulatorConfigs(event.getInsulators());
-        logRegistryLoaded(String.format("Loaded %s insulators", event.getInsulators().size()), event.getInsulators());
+        Collection<Holder<InsulatorData>> insulators = event.getRegistry(ModRegistries.INSULATOR_DATA);
+        addInsulatorConfigs(insulators);
+        logRegistryLoaded(String.format("Loaded %s insulators", insulators.size()), insulators);
         // fuels
-        addFuelConfigs(event.getFuels());
-        logRegistryLoaded(String.format("Loaded %s fuels", event.getFuels().size()), event.getFuels());
+        Collection<Holder<FuelData>> fuels = event.getRegistry(ModRegistries.FUEL_DATA);
+        addFuelConfigs(fuels);
+        logRegistryLoaded(String.format("Loaded %s fuels", fuels.size()), fuels);
         // foods
-        addFoodConfigs(event.getFoods());
-        logRegistryLoaded(String.format("Loaded %s foods", event.getFoods().size()), event.getFoods());
+        Collection<Holder<FoodData>> foods = event.getRegistry(ModRegistries.FOOD_DATA);
+        addFoodConfigs(foods);
+        logRegistryLoaded(String.format("Loaded %s foods", foods.size()), foods);
         // carry temperatures
-        addCarryTempConfigs(event.getCarryTemps());
-        logRegistryLoaded(String.format("Loaded %s carried item temperatures", event.getCarryTemps().size()), event.getCarryTemps());
+        Collection<Holder<ItemCarryTempData>> carryTemps = event.getRegistry(ModRegistries.CARRY_TEMP_DATA);
+        addCarryTempConfigs(carryTemps);
+        logRegistryLoaded(String.format("Loaded %s carried item temperatures", carryTemps.size()), carryTemps);
+        // drying items
+        Collection<Holder<DryingItemData>> dryingItems = event.getRegistry(ModRegistries.DRYING_ITEM_DATA);
+        addDryingItemConfigs(dryingItems);
+        logRegistryLoaded(String.format("Loaded %s drying items", dryingItems.size()), dryingItems);
 
         // block temperatures
-        addBlockTempConfigs(event.getBlockTemps());
-        logRegistryLoaded(String.format("Loaded %s block temperatures", event.getBlockTemps().size()), event.getBlockTemps());
+        Collection<Holder<BlockTempData>> blockTemps = event.getRegistry(ModRegistries.BLOCK_TEMP_DATA);
+        addBlockTempConfigs(blockTemps);
+        logRegistryLoaded(String.format("Loaded %s block temperatures", blockTemps.size()), blockTemps);
         // biome temperatures
-        addBiomeTempConfigs(event.getBiomeTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s biome temperatures", event.getBiomeTemps().size()), event.getBiomeTemps());
+        Collection<Holder<BiomeTempData>> biomeTemps = event.getRegistry(ModRegistries.BIOME_TEMP_DATA);
+        addBiomeTempConfigs(biomeTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s biome temperatures", biomeTemps.size()), biomeTemps);
         // dimension temperatures
-        addDimensionTempConfigs(event.getDimensionTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s dimension temperatures", event.getDimensionTemps().size()), event.getDimensionTemps());
+        Collection<Holder<DimensionTempData>> dimensionTemps = event.getRegistry(ModRegistries.DIMENSION_TEMP_DATA);
+        addDimensionTempConfigs(dimensionTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s dimension temperatures", dimensionTemps.size()), dimensionTemps);
         // structure temperatures
-        addStructureTempConfigs(event.getStructureTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s structure temperatures", event.getStructureTemps().size()), event.getStructureTemps());
+        Collection<Holder<StructureTempData>> structureTemps = event.getRegistry(ModRegistries.STRUCTURE_TEMP_DATA);
+        addStructureTempConfigs(structureTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s structure temperatures", structureTemps.size()), structureTemps);
         // depth temperatures
-        addDepthTempConfigs(event.getDepthTemps());
-        logRegistryLoaded(String.format("Loaded %s depth temperatures", event.getDepthTemps().size()), event.getDepthTemps());
+        Collection<Holder<DepthTempData>> depthTemps = event.getRegistry(ModRegistries.DEPTH_TEMP_DATA);
+        addDepthTempConfigs(depthTemps);
+        logRegistryLoaded(String.format("Loaded %s depth temperatures", depthTemps.size()), depthTemps);
 
         // mounts
-        addMountConfigs(event.getMounts());
-        logRegistryLoaded(String.format("Loaded %s insulated mounts", event.getMounts().size()), event.getMounts());
+        Collection<Holder<MountData>> mounts = event.getRegistry(ModRegistries.MOUNT_DATA);
+        addMountConfigs(mounts);
+        logRegistryLoaded(String.format("Loaded %s insulated mounts", mounts.size()), mounts);
         // spawn biomes
-        addSpawnBiomeConfigs(event.getSpawnBiomes(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s entity spawn biomes", event.getSpawnBiomes().size()), event.getSpawnBiomes());
+        Collection<Holder<SpawnBiomeData>> spawnBiomes = event.getRegistry(ModRegistries.ENTITY_SPAWN_BIOME_DATA);
+        addSpawnBiomeConfigs(spawnBiomes, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s entity spawn biomes", spawnBiomes.size()), spawnBiomes);
         // entity temperatures
-        addEntityTempConfigs(event.getEntityTemps());
-        logRegistryLoaded(String.format("Loaded %s entity temperatures", event.getEntityTemps().size()), event.getEntityTemps());
+        Collection<Holder<EntityTempData>> entityTemps = event.getRegistry(ModRegistries.ENTITY_TEMP_DATA);
+        addEntityTempConfigs(entityTemps);
+        logRegistryLoaded(String.format("Loaded %s entity temperatures", entityTemps.size()), entityTemps);
+
+        CreateRegistriesEvent.Post postEvent = new CreateRegistriesEvent.Post(registryAccess, event.getRegistries());
+        MinecraftForge.EVENT_BUS.post(postEvent);
     }
 
     private static void logRegistryLoaded(String message, Collection<?> registry)
@@ -452,6 +471,29 @@ public class ConfigLoadingHandler
             });
             for (Item item : items)
             {   ConfigSettings.CARRIED_ITEM_TEMPERATURES.get().put(item, carryTempData);
+            }
+        });
+    }
+
+    private static void addDryingItemConfigs(Collection<Holder<DryingItemData>> dryingItems)
+    {
+        dryingItems.forEach(holder ->
+        {
+            DryingItemData dryingItemData = holder.get();
+            // Check if the required mods are loaded
+            if (!dryingItemData.areRequiredModsLoaded())
+            {   return;
+            }
+
+            List<Item> items = new ArrayList<>();
+            dryingItemData.data().items().ifPresent(itemList ->
+            {   items.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, itemList));
+            });
+            dryingItemData.data().tag().ifPresent(tag ->
+            {   items.addAll(ForgeRegistries.ITEMS.tags().getTag(tag).stream().toList());
+            });
+            for (Item item : items)
+            {   ConfigSettings.DRYING_ITEMS.get().put(item, dryingItemData);
             }
         });
     }

src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -36,7 +36,6 @@ import net.minecraftforge.fml.util.thread.EffectiveSide;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.util.TriConsumer;
 import org.joml.Vector2i;
-import oshi.util.tuples.Triplet;
 
 import java.util.*;
 import java.util.function.*;
@@ -127,6 +126,7 @@ public class ConfigSettings
     public static final DynamicHolder<Multimap<Item, InsulatorData>> INSULATING_CURIOS;
     public static final DynamicHolder<ScalingFormula> INSULATION_SLOTS;
     public static final DynamicHolder<List<Item>> INSULATION_BLACKLIST;
+    public static final DynamicHolder<Map<Item, DryingItemData>> DRYING_ITEMS;
 
     public static final DynamicHolder<Multimap<Item, FoodData>> FOOD_TEMPERATURES;
 
@@ -490,6 +490,30 @@ public class ConfigSettings
                                                     .stream()
                                                     .map(entry -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(entry)))
                                                     .collect(ArrayList::new, List::add, List::addAll)));
+
+        DRYING_ITEMS = addSyncedSetting("drying_items", FastMap::new, holder ->
+        {
+            Map<Item, DryingItemData> dataMap = new FastMap<>();
+            for (List<?> entry : ItemSettingsConfig.DRYING_ITEMS.get())
+            {
+                DryingItemData data = DryingItemData.fromToml(entry);
+                if (data == null) continue;
+
+                data.setType(ConfigData.Type.TOML);
+
+                for (Item item : RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ITEMS, CSMath.listOrEmpty(data.data().items())))
+                {   dataMap.put(item, data);
+                }
+            }
+            // Handle registry removals
+            ConfigLoadingHandler.removeEntries(dataMap.values(), ModRegistries.DRYING_ITEM_DATA);
+            // Add entries
+            holder.get().putAll(dataMap);
+        },
+        (encoder) -> ConfigHelper.serializeRegistry(encoder, "DryingItems", Registries.ITEM, ModRegistries.DRYING_ITEM_DATA, ForgeRegistries.ITEMS::getKey),
+        (decoder) -> ConfigHelper.deserializeRegistry(decoder, "DryingItems", ModRegistries.DRYING_ITEM_DATA, rl -> ForgeRegistries.ITEMS.getValue(rl)),
+        (saver) -> {},
+        SyncType.ONE_WAY);
 
         CHECK_SLEEP_CONDITIONS = addSetting("check_sleep_conditions", () -> true, holder -> holder.set(WorldSettingsConfig.SHOULD_CHECK_SLEEP.get()));
 

src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
@@ -37,6 +37,8 @@ public class ItemSettingsConfig
     public static final ForgeConfigSpec.IntValue WATERSKIN_STRENGTH;
     public static final ForgeConfigSpec.DoubleValue SOULSPRING_LAMP_STRENGTH;
 
+    public static final ForgeConfigSpec.ConfigValue<List<? extends List<?>>> DRYING_ITEMS;
+
     public static final ForgeConfigSpec.ConfigValue<List<? extends List<?>>> INSULATING_CURIOS;
 
     static
@@ -277,6 +279,14 @@ public class ItemSettingsConfig
                 .comment("Determines the strength of the Soulspring Lamp's effect before it is overwhelmed",
                          "A value of 1 means it will never be overwhelmed")
                 .defineInRange("Soulspring Lamp Strength", 0.6, 0, 1);
+
+        DRYING_ITEMS = BUILDER
+                .comment("Defines items that can be used to dry the player",
+                        "Format: [[\"item_id\", \"turns_into\"], [\"item_id\", \"turns_into\"], ...etc]")
+                .defineListAllowEmpty(List.of("Drying Items"), () -> List.of(
+                        List.of("minecraft:sponge", "minecraft:wet_sponge")
+                ),
+                it -> it instanceof String);
 
         BUILDER.pop();
 

MikulGamour/Cold-Sweat-Rework-Forge

src/main/java/com/momosoftworks/coldsweat/config/ConfigLoadingHandler.java
@@ -223,43 +223,62 @@ public class ConfigLoadingHandler
          Add JSON data to the config settings
          */
         // insulators
-        addInsulatorConfigs(event.getInsulators());
-        logRegistryLoaded(String.format("Loaded %s insulators", event.getInsulators().size()), event.getInsulators());
+        Collection<Holder<InsulatorData>> insulators = event.getRegistry(ModRegistries.INSULATOR_DATA);
+        addInsulatorConfigs(insulators);
+        logRegistryLoaded(String.format("Loaded %s insulators", insulators.size()), insulators);
         // fuels
-        addFuelConfigs(event.getFuels());
-        logRegistryLoaded(String.format("Loaded %s fuels", event.getFuels().size()), event.getFuels());
+        Collection<Holder<FuelData>> fuels = event.getRegistry(ModRegistries.FUEL_DATA);
+        addFuelConfigs(fuels);
+        logRegistryLoaded(String.format("Loaded %s fuels", fuels.size()), fuels);
         // foods
-        addFoodConfigs(event.getFoods());
-        logRegistryLoaded(String.format("Loaded %s foods", event.getFoods().size()), event.getFoods());
+        Collection<Holder<FoodData>> foods = event.getRegistry(ModRegistries.FOOD_DATA);
+        addFoodConfigs(foods);
+        logRegistryLoaded(String.format("Loaded %s foods", foods.size()), foods);
         // carry temperatures
-        addCarryTempConfigs(event.getCarryTemps());
-        logRegistryLoaded(String.format("Loaded %s carried item temperatures", event.getCarryTemps().size()), event.getCarryTemps());
+        Collection<Holder<ItemCarryTempData>> carryTemps = event.getRegistry(ModRegistries.CARRY_TEMP_DATA);
+        addCarryTempConfigs(carryTemps);
+        logRegistryLoaded(String.format("Loaded %s carried item temperatures", carryTemps.size()), carryTemps);
+        // drying items
+        Collection<Holder<DryingItemData>> dryingItems = event.getRegistry(ModRegistries.DRYING_ITEM_DATA);
+        addDryingItemConfigs(dryingItems);
+        logRegistryLoaded(String.format("Loaded %s drying items", dryingItems.size()), dryingItems);
 
         // block temperatures
-        addBlockTempConfigs(event.getBlockTemps());
-        logRegistryLoaded(String.format("Loaded %s block temperatures", event.getBlockTemps().size()), event.getBlockTemps());
+        Collection<Holder<BlockTempData>> blockTemps = event.getRegistry(ModRegistries.BLOCK_TEMP_DATA);
+        addBlockTempConfigs(blockTemps);
+        logRegistryLoaded(String.format("Loaded %s block temperatures", blockTemps.size()), blockTemps);
         // biome temperatures
-        addBiomeTempConfigs(event.getBiomeTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s biome temperatures", event.getBiomeTemps().size()), event.getBiomeTemps());
+        Collection<Holder<BiomeTempData>> biomeTemps = event.getRegistry(ModRegistries.BIOME_TEMP_DATA);
+        addBiomeTempConfigs(biomeTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s biome temperatures", biomeTemps.size()), biomeTemps);
         // dimension temperatures
-        addDimensionTempConfigs(event.getDimensionTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s dimension temperatures", event.getDimensionTemps().size()), event.getDimensionTemps());
+        Collection<Holder<DimensionTempData>> dimensionTemps = event.getRegistry(ModRegistries.DIMENSION_TEMP_DATA);
+        addDimensionTempConfigs(dimensionTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s dimension temperatures", dimensionTemps.size()), dimensionTemps);
         // structure temperatures
-        addStructureTempConfigs(event.getStructureTemps(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s structure temperatures", event.getStructureTemps().size()), event.getStructureTemps());
+        Collection<Holder<StructureTempData>> structureTemps = event.getRegistry(ModRegistries.STRUCTURE_TEMP_DATA);
+        addStructureTempConfigs(structureTemps, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s structure temperatures", structureTemps.size()), structureTemps);
         // depth temperatures
-        addDepthTempConfigs(event.getDepthTemps());
-        logRegistryLoaded(String.format("Loaded %s depth temperatures", event.getDepthTemps().size()), event.getDepthTemps());
+        Collection<Holder<DepthTempData>> depthTemps = event.getRegistry(ModRegistries.DEPTH_TEMP_DATA);
+        addDepthTempConfigs(depthTemps);
+        logRegistryLoaded(String.format("Loaded %s depth temperatures", depthTemps.size()), depthTemps);
 
         // mounts
-        addMountConfigs(event.getMounts());
-        logRegistryLoaded(String.format("Loaded %s insulated mounts", event.getMounts().size()), event.getMounts());
+        Collection<Holder<MountData>> mounts = event.getRegistry(ModRegistries.MOUNT_DATA);
+        addMountConfigs(mounts);
+        logRegistryLoaded(String.format("Loaded %s insulated mounts", mounts.size()), mounts);
         // spawn biomes
-        addSpawnBiomeConfigs(event.getSpawnBiomes(), registryAccess);
-        logRegistryLoaded(String.format("Loaded %s entity spawn biomes", event.getSpawnBiomes().size()), event.getSpawnBiomes());
+        Collection<Holder<SpawnBiomeData>> spawnBiomes = event.getRegistry(ModRegistries.ENTITY_SPAWN_BIOME_DATA);
+        addSpawnBiomeConfigs(spawnBiomes, registryAccess);
+        logRegistryLoaded(String.format("Loaded %s entity spawn biomes", spawnBiomes.size()), spawnBiomes);
         // entity temperatures
-        addEntityTempConfigs(event.getEntityTemps());
-        logRegistryLoaded(String.format("Loaded %s entity temperatures", event.getEntityTemps().size()), event.getEntityTemps());
+        Collection<Holder<EntityTempData>> entityTemps = event.getRegistry(ModRegistries.ENTITY_TEMP_DATA);
+        addEntityTempConfigs(entityTemps);
+        logRegistryLoaded(String.format("Loaded %s entity temperatures", entityTemps.size()), entityTemps);
+
+        CreateRegistriesEvent.Post postEvent = new CreateRegistriesEvent.Post(registryAccess, event.getRegistries());
+        NeoForge.EVENT_BUS.post(postEvent);
     }
 
     private static void logRegistryLoaded(String message, Collection<?> registry)
@@ -453,6 +472,29 @@ public class ConfigLoadingHandler
             });
             for (Item item : items)
             {   ConfigSettings.CARRIED_ITEM_TEMPERATURES.get().put(item, carryTempData);
+            }
+        });
+    }
+
+    private static void addDryingItemConfigs(Collection<Holder<DryingItemData>> dryingItems)
+    {
+        dryingItems.forEach(holder ->
+        {
+            DryingItemData dryingItemData = holder.value();
+            // Check if the required mods are loaded
+            if (!dryingItemData.areRequiredModsLoaded())
+            {   return;
+            }
+
+            List<Item> items = new ArrayList<>();
+            dryingItemData.data().items().ifPresent(itemList ->
+            {   items.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ITEM, itemList));
+            });
+            dryingItemData.data().tag().ifPresent(tag ->
+            {   items.addAll(BuiltInRegistries.ITEM.getTag(tag).get().stream().map(Holder::value).toList());
+            });
+            for (Item item : items)
+            {   ConfigSettings.DRYING_ITEMS.get().put(item, dryingItemData);
             }
         });
     }

src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -36,7 +36,6 @@ import net.neoforged.fml.util.thread.EffectiveSide;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.logging.log4j.util.TriConsumer;
 import org.joml.Vector2i;
-import oshi.util.tuples.Triplet;
 
 import java.util.*;
 import java.util.function.*;
@@ -127,6 +126,7 @@ public class ConfigSettings
     public static final DynamicHolder<Multimap<Item, InsulatorData>> INSULATING_CURIOS;
     public static final DynamicHolder<ScalingFormula> INSULATION_SLOTS;
     public static final DynamicHolder<List<Item>> INSULATION_BLACKLIST;
+    public static final DynamicHolder<Map<Item, DryingItemData>> DRYING_ITEMS;
 
     public static final DynamicHolder<Multimap<Item, FoodData>> FOOD_TEMPERATURES;
 
@@ -490,6 +490,30 @@ public class ConfigSettings
                                                     .stream()
                                                     .map(entry -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(entry)))
                                                     .collect(ArrayList::new, List::add, List::addAll)));
+
+        DRYING_ITEMS = addSyncedSetting("drying_items", FastMap::new, holder ->
+        {
+            Map<Item, DryingItemData> dataMap = new FastMap<>();
+            for (List<?> entry : ItemSettingsConfig.DRYING_ITEMS.get())
+            {
+                DryingItemData data = DryingItemData.fromToml(entry);
+                if (data == null) continue;
+
+                data.setType(ConfigData.Type.TOML);
+
+                for (Item item : RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ITEM, CSMath.listOrEmpty(data.data().items())))
+                {   dataMap.put(item, data);
+                }
+            }
+            // Handle registry removals
+            ConfigLoadingHandler.removeEntries(dataMap.values(), ModRegistries.DRYING_ITEM_DATA);
+            // Add entries
+            holder.get().putAll(dataMap);
+        },
+        (encoder) -> ConfigHelper.serializeRegistry(encoder, "DryingItems", Registries.ITEM, ModRegistries.DRYING_ITEM_DATA, BuiltInRegistries.ITEM::getKey),
+        (decoder) -> ConfigHelper.deserializeRegistry(decoder, "DryingItems", ModRegistries.DRYING_ITEM_DATA, rl -> BuiltInRegistries.ITEM.get(rl)),
+        (saver) -> {},
+        SyncType.ONE_WAY);
 
         CHECK_SLEEP_CONDITIONS = addSetting("check_sleep_conditions", () -> true, holder -> holder.set(WorldSettingsConfig.SHOULD_CHECK_SLEEP.get()));
 

src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
@@ -38,6 +38,8 @@ public class ItemSettingsConfig
     public static final ModConfigSpec.IntValue WATERSKIN_STRENGTH;
     public static final ModConfigSpec.DoubleValue SOULSPRING_LAMP_STRENGTH;
 
+    public static final ModConfigSpec.ConfigValue<List<? extends List<?>>> DRYING_ITEMS;
+
     public static final ModConfigSpec.ConfigValue<List<? extends List<?>>> INSULATING_CURIOS;
 
     static
@@ -278,6 +280,14 @@ public class ItemSettingsConfig
                 .comment("Determines the strength of the Soulspring Lamp's effect before it is overwhelmed",
                          "A value of 1 means it will never be overwhelmed")
                 .defineInRange("Soulspring Lamp Strength", 0.6, 0, 1);
+
+        DRYING_ITEMS = BUILDER
+                .comment("Defines items that can be used to dry the player",
+                        "Format: [[\"item_id\", \"turns_into\"], [\"item_id\", \"turns_into\"], ...etc]")
+                .defineListAllowEmpty(List.of("Drying Items"), () -> List.of(
+                        List.of("minecraft:sponge", "minecraft:wet_sponge")
+                ),
+                it -> it instanceof String);
 
         BUILDER.pop();
 