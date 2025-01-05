1266aea2ea32eb5494f72296aef84aedda9b756c
MikulDev <gamer3mc@gmail.com>
2025-01-04 19:35:15 -0500

    Fix insulator tooltip syncing on dedicated servers
    Took 1 second

--- src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
+++ src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
@@ -213,8 +213,13 @@ public class SewingContainer extends ItemCombinerMenu
             {
                 ItemStack processed = wearableItem.copy();
                 if (insulateArmorItem(processed, insulatorItem))
-                {   this.setItem(this.getResultSlot(), processed);
-                    this.broadcastChanges();
+                {
+                    // Serialize insulation data for client syncing
+                    ItemInsulationManager.getInsulationCap(wearableItem).ifPresent(cap ->
+                    {   processed.getOrCreateTag().merge(cap.serializeNBT());
+                    });
+                    // Set slot to result
+                    this.setItem(this.getResultSlot(), processed);
                 }
             }
         }
