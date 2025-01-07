706ddc93d38ac1db50603da3fc0dadadac871b5e
MikulDev <gamer3mc@gmail.com>
2025-01-07 14:21:46 -0500

    Remove MixinFreezingWater$TickSpeed mixin

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinFreezingWater.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinFreezingWater.java
@@ -55,25 +55,6 @@ public class MixinFreezingWater
         cir.setReturnValue(false);
     }
 
-    @Mixin(value = ServerLevel.class)
-    public static abstract class FreezeTickSpeed
-    {
-        ServerLevel self = (ServerLevel) (Object) this;
-
-        @ModifyArg(method = "tickChunk", at = @At(target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", value = "INVOKE"),
-                   slice = @Slice(from = @At(target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V", value = "INVOKE", ordinal = 0),
-                                  to = @At(target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V", value = "INVOKE", ordinal = 1)),
-                   require = 0)
-        private int tickFreezeSpeed(int bound)
-        {
-            if (!ConfigSettings.USE_CUSTOM_WATER_FREEZE_BEHAVIOR.get()) return bound;
-
-            int tickSpeed = self.getGameRules().getInt(GameRules.RULE_RANDOMTICKING);
-            if (tickSpeed == 0) return 999999;
-            return Math.max(1, bound / Math.max(1, tickSpeed / 3));
-        }
-    }
-
     @Mixin(IceBlock.class)
     public static abstract class IceMelt
     {
