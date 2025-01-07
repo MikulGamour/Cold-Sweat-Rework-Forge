4aa3893a2360f9595349dde6ad1c022f456719f0
MikulDev <gamer3mc@gmail.com>
2025-01-07 15:08:02 -0500

    Fix MixinHeartRender

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
@@ -3,7 +3,7 @@ package com.momosoftworks.coldsweat.mixin;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.momosoftworks.coldsweat.ColdSweat;
-import com.momosoftworks.coldsweat.common.event.TempEffectsCommon;
+import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.client.gui.Overlays;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.math.CSMath;
@@ -29,50 +29,42 @@ public class MixinHeartRender
     private static int HEART_INDEX = 0;
 
     @Inject(method = "renderHeart(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Gui$HeartType;IIIZZ)V", at = @At("TAIL"), cancellable = true)
-    private void renderHeart(PoseStack ps, Gui.HeartType heartType, int x, int y, int texV, boolean blink, boolean half, CallbackInfo ci)
+    private void renderHeart(PoseStack ps, Gui.HeartType heartType, int x, int y, int texV, boolean blink, boolean halfHeart, CallbackInfo ci)
     {
-        double heartsFreezePercentage = ConfigSettings.HEARTS_FREEZING_PERCENTAGE.get();
-        Player player = Minecraft.getInstance().player;
-        if (player == null) return;
-
-        boolean isHardcore = player.level.getLevelData().isHardcore();
-
-        if (heartsFreezePercentage == 0
-        || player.hasEffect(ModEffects.GRACE)) return;
-
         // This check ensures that this only gets called once per heart
         if (heartType == Gui.HeartType.CONTAINER)
         {   HEART_INDEX += 1;
         }
+        double heartsFreezePercentage = ConfigSettings.HEARTS_FREEZING_PERCENTAGE.get();
+        Player player = Minecraft.getInstance().player;
 
+        if (player == null) return;
+        if (heartsFreezePercentage == 0 || player.hasEffect(ModEffects.GRACE)) return;
         if (player.hasEffect(ModEffects.ICE_RESISTANCE)) return;
+
         double temp = Overlays.BODY_TEMP;
+        float maxHealth = player.getMaxHealth();
+        boolean isHardcore = player.level.getLevelData().isHardcore();
 
         // Get protection from armor underwear
-        float unfrozenHealth = CSMath.blend((float) (1 - heartsFreezePercentage), 1, TempEffectsCommon.getColdResistance(player), 0, 4);
-        if (unfrozenHealth == 1) return;
+        float maxFrozenHealth = (float) CSMath.blend(maxHealth * heartsFreezePercentage, 0, Temperature.get(player, Temperature.Trait.COLD_RESISTANCE), 0, 1);
+        if (maxFrozenHealth == 0) return;
 
-        int frozenHealth = (int) (player.getMaxHealth() - player.getMaxHealth() * CSMath.blend(unfrozenHealth, 1, temp, -100, -50));
-        int frozenHearts = frozenHealth / 2;
-        int u = blink || heartType == Gui.HeartType.CONTAINER ? 14 : half ? 7 : 0;
+        int frozenHealth = (int) CSMath.blend(0, maxHealth * heartsFreezePercentage, temp, -50, -100);
+        int frozenHearts = Math.round(frozenHealth / 2f);
+        boolean partialFrozen = frozenHealth % 2 == 1 && HEART_INDEX == frozenHearts;
+        int u = isHardcore ? 7 : 0;
+        int v = partialFrozen ? halfHeart ? 21 : 14 : halfHeart ? 7 : 0;
 
         // Render frozen hearts
         RenderSystem.setShaderTexture(0, HEART_TEXTURE);
-            if (HEART_INDEX > 0 && HEART_INDEX < frozenHearts + 1)
-            {
-                GuiComponent.blit(ps, x, y, 21, 0, 9, 9, 30, 14);
-            GuiComponent.blit(ps, x + 1, y + 1, u, 0, 7, 7, 30, 14);
-            if (isHardcore)
-            {   GuiComponent.blit(ps, x + 1, y + 1, 23, 10, 7, 4, 30, 14);
-            }
-            ci.cancel();
-        }
-        // Render half-frozen heart if needed
-        else if (HEART_INDEX == frozenHearts + 1 && frozenHealth % 2 == 1)
+        if (HEART_INDEX <= frozenHearts)
         {
-            GuiComponent.blit(ps, x + 1, y + 1, u, 7, 7, 7, 30, 14);
-            if (isHardcore)
-            {   GuiComponent.blit(ps, x + 4, y + 1, 26, 10, 4, 4, 30, 14);
+            if (heartType == Gui.HeartType.CONTAINER)
+            {   GuiComponent.blit(ps, x + 1, y + 1, 14, v, 7, 7, 21, 28);
+            }
+            else
+            {  GuiComponent.blit(ps, x + 1, y + 1, u, v, 7, 7, 21, 28);
             }
         }
         RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
