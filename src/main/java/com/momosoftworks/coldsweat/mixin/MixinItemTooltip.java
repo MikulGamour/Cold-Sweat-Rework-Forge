7c1f8f0ef04c28e07fa00667f107f2dad2537235
MikulDev <gamer3mc@gmail.com>
2025-01-22 12:21:25 -0500

    Fix attribute modifiers always showing the insulation icon
    Took 2 seconds

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinItemTooltip.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinItemTooltip.java
@@ -160,7 +160,7 @@ public class MixinItemTooltip
 
             if (EntityTempManager.isTemperatureAttribute(ATTRIBUTE))
             {
-                MutableComponent newline = TooltipHandler.getFormattedAttributeModifier(ATTRIBUTE, MODIFIER.getAmount(), MODIFIER.getOperation(), true, hasUnmetRequirements);
+                MutableComponent newline = TooltipHandler.getFormattedAttributeModifier(ATTRIBUTE, MODIFIER.getAmount(), MODIFIER.getOperation(), isFromInsulation, hasUnmetRequirements);
 
                 for (Component sibling : component.getSiblings())
                 {   newline = newline.append(sibling);
