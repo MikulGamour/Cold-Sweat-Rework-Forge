983e36849d5c31fe216738b21b552dbb00b91c4d
MikulDev <gamer3mc@gmail.com>
2025-02-07 10:46:55 -0500

    Add CreateRegistriesEvent.Pre to prevent double-firing when subscribing to main event
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/compat/kubejs/KubePlugin.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/kubejs/KubePlugin.java
@@ -26,7 +26,7 @@ public class KubePlugin extends KubeJSPlugin
     }
 
     @SubscribeEvent
-    public static void fireRegistries(CreateRegistriesEvent event)
+    public static void fireRegistries(CreateRegistriesEvent.Pre event)
     {   KubeEventSignatures.REGISTRIES.invoker().buildRegistries();
     }
 
