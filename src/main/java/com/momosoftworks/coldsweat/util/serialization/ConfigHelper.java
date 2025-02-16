ebe139f5bdfd3b678266749b61a3c08a599a702d
MikulDev <gamer3mc@gmail.com>
2025-02-15 23:43:50 -0500

    Use Cold Sweat logger for exception throws

--- src/main/java/com/momosoftworks/coldsweat/util/serialization/ConfigHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/serialization/ConfigHelper.java
@@ -550,8 +550,7 @@ public class ConfigHelper
     public static <T> Optional<T> parseResource(ResourceManager resourceManager, ResourceLocation location, Codec<T> codec)
     {
         if (resourceManager == null)
-        {
-            return Optional.empty();
+        {   return Optional.empty();
         }
         try
         {
@@ -563,8 +562,8 @@ public class ConfigHelper
             }
         }
         catch (IOException e)
-        {
-            throw new RuntimeException("Failed to load JSON file: " + location, e);
+        {   ColdSweat.LOGGER.error(new RuntimeException("Failed to load JSON file: " + location, e));
+            return Optional.empty();
         }
     }
 
