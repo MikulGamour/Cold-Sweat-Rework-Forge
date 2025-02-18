a34f19ae4c547c59c6781ecf6842f0a0ef6f3660
MikulDev <gamer3mc@gmail.com>
2025-02-18 13:08:17 -0500

    Make boiler recipe override other cobble-based recipes
    Took 5 minutes

--- src/main/java/com/momosoftworks/coldsweat/common/event/BoilerRecipeOverride.java
+++ src/main/java/com/momosoftworks/coldsweat/common/event/BoilerRecipeOverride.java
@@ -1,16 +1,17 @@
 package com.momosoftworks.coldsweat.common.event;
 
 import com.momosoftworks.coldsweat.ColdSweat;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.inventory.container.*;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.Container;
-import net.minecraft.world.inventory.*;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
@@ -21,46 +22,51 @@ public class BoilerRecipeOverride
     @SubscribeEvent
     public static void onCraftingTableOpen(PlayerContainerEvent.Open event)
     {
-        if (event.getContainer() instanceof RecipeBookMenu<?> crafting
-        && crafting.getGridWidth() == 3 && crafting.getGridHeight() == 3)
+        if (event.getContainer() instanceof RecipeBookContainer<?>
+        && ((RecipeBookContainer<?>) event.getContainer()).getGridWidth() == 3
+        && ((RecipeBookContainer<?>) event.getContainer()).getGridHeight() == 3)
         {
+            RecipeBookContainer<?> crafting = (RecipeBookContainer<?>) event.getContainer();
             MinecraftServer server = event.getEntity().getServer();
             if (server == null) return;
-            Recipe<Container> boilerRecipe = (Recipe) server.getRecipeManager().byKey(new ResourceLocation(ColdSweat.MOD_ID, "boiler")).orElse(null);
+            IRecipe<CraftingInventory> boilerRecipe = (IRecipe) server.getRecipeManager().byKey(new ResourceLocation(ColdSweat.MOD_ID, "boiler")).orElse(null);
             if (boilerRecipe == null) return;
             
-            crafting.addSlotListener(new ContainerListener()
+            crafting.addSlotListener(new IContainerListener()
             {
                 @Override
-                public void slotChanged(AbstractContainerMenu sendingContainer, int slotId, ItemStack stack)
+                public void slotChanged(Container sendingContainer, int slotId, ItemStack stack)
                 {
                     Slot slot = sendingContainer.getSlot(slotId);
 
-                    if (slot instanceof ResultSlot resultSlot)
+                    if (slot instanceof CraftingResultSlot)
                     {
-                        if (crafting.recipeMatches(boilerRecipe))
-                        {   slot.set(boilerRecipe.assemble(getCraftingContainer(resultSlot), server.registryAccess()));
+                        if (crafting.recipeMatches((IRecipe) boilerRecipe))
+                        {   slot.set(boilerRecipe.assemble(getCraftingContainer(slot)));
                         }
                     }
                 }
 
                 @Override
-                public void dataChanged(AbstractContainerMenu pContainerMenu, int pDataSlotIndex, int pValue)
+                public void setContainerData(Container pContainerMenu, int pDataSlotIndex, int pValue)
+                {}
+                @Override
+                public void refreshContainer(Container pContainerToSend, NonNullList<ItemStack> pItemsList)
                 {}
             });
         }
     }
 
-    private static final Field SLOT_CRAFT_CONTAINER = ObfuscationReflectionHelper.findField(ResultSlot.class, "f_40162_");
+    private static final Field SLOT_CRAFT_CONTAINER = ObfuscationReflectionHelper.findField(CraftingResultSlot.class, "field_75239_a");
     static
     {   SLOT_CRAFT_CONTAINER.setAccessible(true);
     }
 
     @Nullable
-    private static CraftingContainer getCraftingContainer(ResultSlot slot)
+    private static CraftingInventory getCraftingContainer(Slot slot)
     {
         try
-        {   return (CraftingContainer) SLOT_CRAFT_CONTAINER.get(slot);
+        {   return (CraftingInventory) SLOT_CRAFT_CONTAINER.get(slot);
         }
         catch (IllegalAccessException e)
         {   ColdSweat.LOGGER.error("Failed to get crafting container from ResultSlot", e);
